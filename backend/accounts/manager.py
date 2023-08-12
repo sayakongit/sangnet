6510871ff2775b2d7b4f98878550b6249d4e2711
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-08-12 13:45:30 +0530

    feat: :sparkles: accounts_module_added

---
+++ backend/accounts/manager.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+      
+        if not email:
+            raise ValueError('Email is required')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+      
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        
+        return self.create_user(email, password, **extra_fields)
