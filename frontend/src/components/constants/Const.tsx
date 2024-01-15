0f973625bda13e2be6721968988729a1dee1c0b6
Xron Trix <xrontrix@gmail.com>
2024-01-16 02:22:49 +0530

    reusable constants

--- frontend/src/components/constants/Const.tsx
+++ frontend/src/components/constants/Const.tsx
@@ -1,8 +1,27 @@
-
 const URL = process.env.BACKEND_IP;
 
+export const json_header = {
+  "Content-type": "application/json",
+};
+
 // export const backend = `${URL}:8000`;
-export const backend = `http://192.168.0.105:8000`;
+export const backend = `http://192.168.0.103:8000`;
+
+export const apply_donor = `${backend}/donor/apply/`;
+
+export const verify_token = `${backend}/accounts/token/verify/`;
+
+export const verify_otp = `${backend}/accounts/verify/`;
+
+export const req_login = `${backend}/accounts/login/`;
+
+export const req_signup = `${backend}/accounts/register/`;
+
+export const nearby_donor = `${backend}/donor/nearby/`;
+
+export const user_profile = `${backend}/accounts/profile/`;
+
+export const get_location = `${backend}/accounts/location/`
 
 
 // Database URL will be added later
